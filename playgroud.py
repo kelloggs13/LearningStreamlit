85bcd1a63b7236ce3acc3576de0fa247abd4d7cf
Kelloggs13 <tobias@byland.info>
2023-07-25 21:46:58 +0200

    deleted playground.py

--- playgroud.py
+++
@@ -1,5 +0,0 @@
-
-from sklearn.datasets import load_iris
-from sklearn.model_selection import cross_val_score
-from sklearn.tree import DecisionTreeClassifier
-clf = DecisionTreeClassifier(random_state=0)
